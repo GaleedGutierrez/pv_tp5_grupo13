e0d3981cb69afdc049dd90efc19cd5a4a9bb29f1
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-02 16:30:45 -0300

    feat: create main entry point for the React application

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import './index.css';
+import '@ant-design/v5-patch-for-react-19';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App.jsx';
+
+const ROOT = document.getElementById('root');
+
+if (ROOT instanceof HTMLDivElement) {
+	createRoot(ROOT).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
