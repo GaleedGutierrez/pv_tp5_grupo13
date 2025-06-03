7adf3cefb82b15bcaa67c59b8e603398989b6f73
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-03 06:16:53 -0300

    feat: add routing model and student type definition

---
+++ src/models/routers.model.js
@@ -0,0 +1,10 @@
+export const AppRoutes = {
+	home: '/',
+	students: {
+		studentsHome: '/alumnos',
+		studentDetails: '/alumnos/:alumnoId',
+		addNewStudent: '/alumnos/nuevo',
+		updateStudent: '/alumnos/:alumnoId/editar',
+	},
+	teamDetails: '/about',
+};
