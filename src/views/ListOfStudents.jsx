262c0091f2c55f6b775deb5173090e5d4a8555b5
galeed gutierrez <galeedeveloper@gmail.com>
2025-06-04 08:10:29 -0300

    feat: add ListOfStudents component to display a list of students with a table format

---
+++ src/views/ListOfStudents.jsx
@@ -0,0 +1,72 @@
+import { Box, Table, Text } from '@chakra-ui/react';
+import { Navbar } from '@components/NavBar';
+import { TablesTableRow } from '@components/TablesTableRow';
+import { useGlobalContext } from '@context/global.context';
+
+/**
+ * This component renders a simple list of students.
+ * @returns {import('react').JSX.Element} The rendered component with a list of students.
+ */
+export const ListOfStudents = () => {
+	const { students } = useGlobalContext();
+
+	return (
+		<>
+			<header>
+				<Navbar />
+			</header>
+			<main>
+				<Box
+					as="section"
+					p={10}
+				>
+					<Table.Root interactive>
+						<Table.Header>
+							<Table.Row>
+								<Table.ColumnHeader>Alumno</Table.ColumnHeader>
+								<Table.ColumnHeader>L.U.</Table.ColumnHeader>
+								<Table.ColumnHeader>Curso</Table.ColumnHeader>
+								<Table.ColumnHeader>
+									Domicilio
+								</Table.ColumnHeader>
+								<Table.ColumnHeader>
+									Teléfono
+								</Table.ColumnHeader>
+								<Table.ColumnHeader>
+									Acciones
+								</Table.ColumnHeader>
+							</Table.Row>
+						</Table.Header>
+						<Table.Body>
+							{students.length === 0 && (
+								<Table.Row>
+									<Table.ColumnHeader colSpan={6}>
+										<Text textAlign="center">
+											No hay alumnos registrados.
+										</Text>
+									</Table.ColumnHeader>
+								</Table.Row>
+							)}
+							{students.length > 0 &&
+								students.map((student) => (
+									<TablesTableRow
+										key={student.lu}
+										student={student}
+									/>
+								))}
+						</Table.Body>
+						<Table.Footer>
+							<Table.Row>
+								<Table.ColumnHeader colSpan={6}>
+									<Text textAlign="center">
+										Total de alumnos: {students.length}
+									</Text>
+								</Table.ColumnHeader>
+							</Table.Row>
+						</Table.Footer>
+					</Table.Root>
+				</Box>
+			</main>
+		</>
+	);
+};
