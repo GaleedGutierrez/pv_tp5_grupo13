ef546b52cd7e123292ecdc45d51f8f2bf608fdfc
Alfredo Ezequiel Gonzalez Lopez <eg156765@gmail.com>
2025-06-04 23:31:31 -0300

    feat: agregue la vista NotFoundPage

---
+++ src/views/NotFoundPage.jsx
@@ -0,0 +1,66 @@
+import { Button, Center, Grid, Heading, Text } from '@chakra-ui/react';
+import { Footer } from '@components/Footer';
+import { Header } from '@components/Header';
+import { Link, Navigate, Route, Routes } from 'react-router';
+
+/**
+ * NotFoundPage component.
+ * This component renders a simple "not found" page with a title and a message.
+ * @returns {import('react').JSX.Element} The rendered component with routes and a not found page.
+ * */
+const NotFoundPage = () => (
+	<Grid
+		gridTemplateRows="auto 1fr auto"
+		height="100vh"
+	>
+		<Header />
+		<main>
+			<Center
+				flexDirection="column"
+				gap={4}
+				height="93.25vh"
+				verticalAlign="middle"
+			>
+				<Heading
+					color="fg.error"
+					size="6xl"
+				>
+					Ups! Página no encontrada
+				</Heading>
+				<Text textStyle="2xl">
+					Lo sentimos, la página que estás buscando no existe.
+				</Text>
+				<Button
+					asChild
+					colorPalette="blue"
+					fontSize="xl"
+				>
+					<Link to="/">Volver al Inicio</Link>
+				</Button>
+			</Center>
+		</main>
+		<Footer />
+	</Grid>
+);
+
+/**
+ * RoutesWithNotFound component.
+ * This component wraps the provided children routes and adds a catch-all route
+ * that redirects to a "not found" page if no other routes match.
+ * @param {Object} props - The component props.
+ * @param {import('react').ReactNode} props.children - The child routes to render.
+ * @returns {import('react').JSX.Element} The rendered component with routes and a not found page.
+ * */
+export const RoutesWithNotFound = ({ children }) => (
+	<Routes>
+		{children}
+		<Route
+			element={<Navigate to="/404" />}
+			path="*"
+		/>
+		<Route
+			element={<NotFoundPage />}
+			path="/404"
+		/>
+	</Routes>
+);
