7d1aed49496325055756306dacbf501eaff64a7b
Alfredo Ezequiel Gonzalez Lopez <eg156765@gmail.com>
2025-06-04 23:21:15 -0300

    feat: agregue la interfaz estudiantes

---
+++ src/models/student.model.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {object} Student - Represents a student in the system.
+ * @property {string} lu - "Libreta Universitaria" number of the student.
+ * @property {string} name - Name of the student.
+ * @property {string} lastName - Last name of the student.
+ * @property {number} yearOfStudy - Year of study the student is currently in.
+ * @property {string} email - Email address of the student.
+ * @property {string} address - Address of the student.
+ * @property {string} phone - Phone number of the student.
+ */
+
+// Empty export to ensure this file is treated as a module
+export {};
