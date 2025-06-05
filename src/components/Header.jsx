436641de9bd8e47ab082d8f5a138ab23618c72e7
Enzo Isaias Condori <isaias.czv@gmail.com>
2025-06-04 23:05:27 -0300

    feat: cree el componente Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from '@components/NavBar';
+
+/**
+ * Header component that renders the navigation bar.
+ * @returns {import('react').JSX.Element} The rendered header component with the navigation bar.
+ */
+export const Header = () => (
+	<header>
+		<Navbar />
+	</header>
+);
